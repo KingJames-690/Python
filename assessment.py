b984071e70c285389c3229fe95f6dd341961df37
KingJames-690 <budghosh@riccarton.school.nz>
2025-03-25 09:51:21 +1300

    Fixed up the way I did the program due to the fact it was not working at the level I wanted it to be.

--- assessment.py
+++ assessment.py
@@ -1,9 +1,18 @@
 '''This program will ask the user to enter a sea level measurement. Once this is inputed the program will find the average and specify which of the measurements were greater than the average'''
 
-#Create a looping list and ask to enter sea levek input
-todo_list = []
-sea_level = int(input('Enter sea level: '))
-while sea_level:
-  todo_list.append(sea_level)
-  sea_level = int(input('Enter sea level: '))
+#Create a looping list
+measurements = []
 
+#Asking user for input and checking for valid input
+while True:
+    try:
+        sea_level = int(input(f'Enter sea level: '))
+        if sea_level >= 0:
+            break
+        else:
+            print('Invalid input, please enter valid measurements.')
+    except ValueError:
+        print('Invalid input, please enter valid measurements.')
+measurements.append(sea_level)
+
+#When "over" is entered the loop must end. This block of code is for that
